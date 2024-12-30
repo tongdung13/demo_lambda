c74d948d415b4b9629db906d31a24242726e5352
tongdung13 <tongvanthienvu@gmail.com>
2024-12-30 09:28:04 +0700

    o-o

--- src/app.py
+++ src/app.py
@@ -3,8 +3,12 @@ from fastapi import FastAPI
 from routes import demo, profile
 
 app = FastAPI()
-app.include_router(demo.router, prefix="/demo", tags=["Demo"])
 app.include_router(profile.router, prefix="/profile", tags=["Profile"])
+
+
+@app.get("/")
+async def read_root():
+    return demo.hello()
 
 
 handler = Mangum(app)
